15491c6a557ae2323c51eb92efe6b26cac2ec7c7
Marcio Torres <marciojrtorres@gmail.com>
2014-02-24 22:44:13 -0300

    primeiros projetos

---
+++ olamundo/OlaMundo.java
@@ -0,0 +1,11 @@
+// CTRL + SHIFT + P = Localizar comando (sublime)    
+
+public class OlaMundo {
+
+    public static void main(String[] argumentos) {
+
+        System.out.println("Olá Mundo");
+
+    }
+
+}
---
+++ setup/src/setup/Principal.java
@@ -0,0 +1,19 @@
+package setup;
+
+import static java.lang.System.out;
+
+// para compilar:
+// javac -cp bin src\pacote\Classe.java -d bin
+// javac -cp bin src\setup\Principal.java -d bin
+
+// para executar:
+// java -cp bin pacote.Classe
+// java -cp bin setup.Principal
+
+public class Principal {
+    public static void main(String[] args) {
+        
+        out.println("Principal");
+
+    }
+}
