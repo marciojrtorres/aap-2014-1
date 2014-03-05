2b628015e887b8e716f9d8c4f59313c3d5ab950b
Márcio Torres <marciojrtorres@gmail.com>
2014-03-05 14:01:57 -0300

    novas configuracoes para o sublime

--- olamundo/OlaMundo.java
+++ olamundo/OlaMundo.java
@@ -1,11 +1,5 @@
-// CTRL + SHIFT + P = Localizar comando (sublime)    
-
 public class OlaMundo {
-
     public static void main(String[] argumentos) {
-
         System.out.println("Olá Mundo");
-
     }
-
 }
